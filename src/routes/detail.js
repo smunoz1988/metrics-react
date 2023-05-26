2c5fd25bf901b0fce85dbfad068691ef9ada309e
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-05-26 14:27:46 -0500

    update detail style

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,6 +1,7 @@
 import { IoIosArrowBack, IoMdMic } from 'react-icons/io';
-import { FaBeer } from 'react-icons/fa';
 import { AiTwotoneSetting } from 'react-icons/ai';
+import { GiFullMotorcycleHelmet } from 'react-icons/gi';
+import { BsArrowRightCircle } from 'react-icons/bs';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -24,10 +25,10 @@ const Detail = () => {
 
   useEffect(() => {
     const delay = setTimeout(() => {
-      setIsLoading(false); // Update the loading state after the delay
-    }, 1500); // Set the desired delay in milliseconds
+      setIsLoading(false);
+    }, 1500);
 
-    return () => clearTimeout(delay); // Cleanup the timer if the component unmounts
+    return () => clearTimeout(delay);
   }, []);
 
   if (isLoading) {
@@ -37,15 +38,19 @@ const Detail = () => {
           <button className="backButton" type="button" onClick={returntoDrivers}>
             <IoIosArrowBack />
           </button>
-          <p>Driver Standings</p>
+          <p>Driver Detail</p>
           <div>
-            <FaBeer />
-            <FaBeer />
+            <IoMdMic />
+            <AiTwotoneSetting className="setupWheel" />
           </div>
         </div>
-        <div>Loading...</div>
+        <p className="loadingStatus">Loading...</p>
       </>
     );
+  }
+
+  if (!driverInfo) {
+    return <p className="loadingStatus">Loading...</p>;
   }
 
   return (
@@ -60,21 +65,88 @@ const Detail = () => {
           <AiTwotoneSetting className="setupWheel" />
         </div>
       </div>
-      <div>
-        <h2>{`${driverInfo.firstname} ${driverInfo.lastname}`}</h2>
-        <p>{`Points: ${driverInfo.points}`}</p>
+      <div className="titleDriver">
+        <GiFullMotorcycleHelmet className="helmetDriversDetail" />
+        <div className="nameDataDriver">
+          <h2 className="driverTopData">{`${driverInfo.firstname} ${driverInfo.lastname}`}</h2>
+          <p className="driverTopData">{`Points: ${driverInfo.points}`}</p>
+        </div>
       </div>
-      <ul>
-        <li>{`Team: ${driverInfo.team}`}</li>
-        <li>{`Team country: ${driverInfo.country}`}</li>
-        <li>{`GP entered: ${driverInfo.grandsPrixEntered}`}</li>
-        <li>{`Highest Grid Position: ${driverInfo.highestGridPosition}`}</li>
-        <li>{`Podiums: ${driverInfo.podiums}`}</li>
-        <li>{`Highest Race Finish: ${driverInfo.highestRaceFinish}`}</li>
-        <li>{`Place of birth: ${driverInfo.placeOfBirth}`}</li>
-        <li>{`Date of birth: ${driverInfo.dateOfBirth}`}</li>
-        <li>{`World Championship: ${driverInfo.worldChampionships}`}</li>
-      </ul>
+      <p className="statsTitle">DRIVER STATS</p>
+      <div>
+
+        <div className="detailDataContainer">
+          <p>Team:</p>
+          <div className="flexRow">
+            <p>{driverInfo.team}</p>
+            <BsArrowRightCircle className="setupWheel" />
+          </div>
+        </div>
+
+        <div className="detailDataContainer">
+          <p>Team country:</p>
+          <div className="flexRow">
+            <p>{driverInfo.country}</p>
+            <BsArrowRightCircle className="setupWheel" />
+          </div>
+        </div>
+
+        <div className="detailDataContainer">
+          <p>GP entered:</p>
+          <div className="flexRow">
+            <p>{driverInfo.grandsPrixEntered}</p>
+            <BsArrowRightCircle className="setupWheel" />
+          </div>
+        </div>
+
+        <div className="detailDataContainer">
+          <p>Highest Grid Position:</p>
+          <div className="flexRow">
+            <p>{driverInfo.highestGridPosition}</p>
+            <BsArrowRightCircle className="setupWheel" />
+          </div>
+        </div>
+
+        <div className="detailDataContainer">
+          <p>Podiums:</p>
+          <div className="flexRow">
+            <p>{driverInfo.podiums}</p>
+            <BsArrowRightCircle className="setupWheel" />
+          </div>
+        </div>
+
+        <div className="detailDataContainer">
+          <p>Highest Race Finish:</p>
+          <div className="flexRow">
+            <p>{driverInfo.highestRaceFinish}</p>
+            <BsArrowRightCircle className="setupWheel" />
+          </div>
+        </div>
+
+        <div className="detailDataContainer">
+          <p>Place of birth:</p>
+          <div className="flexRow">
+            <p>{driverInfo.placeOfBirth}</p>
+            <BsArrowRightCircle className="setupWheel" />
+          </div>
+        </div>
+
+        <div className="detailDataContainer">
+          <p>Date of birth:</p>
+          <div className="flexRow">
+            <p>{driverInfo.dateOfBirth}</p>
+            <BsArrowRightCircle className="setupWheel" />
+          </div>
+        </div>
+
+        <div className="detailDataContainer">
+          <p>World Championship: </p>
+          <div className="flexRow">
+            <p>{driverInfo.worldChampionships}</p>
+            <BsArrowRightCircle className="setupWheel" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
